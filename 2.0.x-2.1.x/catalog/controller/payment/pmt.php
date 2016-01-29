d5de2a81c443b035f9309732b16a67716e52fb53
pagantis-teamç <fatsifont2@gmail.com>
2016-01-29 10:27:52 +0100

    update return url

--- 2.0.x-2.1.x/catalog/controller/payment/pmt.php
+++ 2.0.x-2.1.x/catalog/controller/payment/pmt.php
@@ -152,36 +152,9 @@ class ControllerPaymentPmt extends Controller
         $this->language->load('payment/pmt');
 
         $this->document->setTitle($this->language->get('heading_fail'));
+        $this->session->data['error'] = $this->language->get('heading_fail');
+        $this->response->redirect($this->url->link('checkout/checkout', '', 'SSL'));
 
-        $data['breadcrumbs'] = array();
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/home'),
-            'separator' => false,
-          );
-
-        $data['heading_title'] = $this->language->get('heading_fail');
-        $data['text_message'] = $this->language->get('message_fail');
-        $data['button_continue'] = $this->language->get('button_continue');
-        $data['continue'] = $this->url->link('checkout/checkout', '', 'SSL');
-
-        if (file_exists(DIR_TEMPLATE.$this->config->get('config_template').'/template/common/success.tpl')) {
-            $this->template = $this->config->get('config_template').'/template/common/success.tpl';
-        } else {
-            $this->template = 'default/template/common/success.tpl';
-        }
-
-        $this->children = array(
-            'common/column_left',
-            'common/column_right',
-            'common/content_top',
-            'common/content_bottom',
-            'common/footer',
-            'common/header',
-        );
-
-        $this->response->setOutput($this->render());
     }
 
     public function success()
