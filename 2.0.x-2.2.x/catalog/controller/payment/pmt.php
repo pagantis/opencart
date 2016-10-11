2363336633b45666f4cae513039abb9bb87466f8
pagantis-teamç <fatsifont2@gmail.com>
2016-10-11 16:11:45 +0200

    add compatibility with opencart 2.2

--- 2.0.x-2.2.x/catalog/controller/payment/pmt.php
+++ 2.0.x-2.2.x/catalog/controller/payment/pmt.php
@@ -148,7 +148,7 @@ class ControllerPaymentPmt extends Controller
         if (file_exists(DIR_TEMPLATE.$this->config->get('config_template').'/template/payment/pmt.tpl')) {
             return $this->load->view($this->config->get('config_template').'/template/payment/pmt.tpl', $data);
         } else {
-            return $this->load->view('default/template/payment/pmt.tpl', $data);
+            return $this->load->view($this->config->get('config_template').'/payment/pmt.tpl', $data);
         }
     }
 
