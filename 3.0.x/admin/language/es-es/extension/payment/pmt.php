8498f465c1d6394ed9c02693200ae422d91a7600
pgarces <gspablo@gmail.com>
2019-10-08 18:22:17 +0200

    Language sentences

---
+++ 3.0.x/admin/language/es-es/extension/payment/pmt.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title'] = 'Paga+Tarde';
+$_['error_pmt_pmt_keys'] = 'Por favor revisa las credenciales';
+$_['text_edit'] = 'Editando la configuración de Paga+Tarde';
+
+// Text
+$_['text_payment'] = 'Pago';
+$_['text_success'] = 'Enhorabuena: Has modificado los detalles de pago';
+$_['text_pmt'] = '<img src="view/image/payment/logo_pmt.png" alt="PagaMasTarde" title="PagaMasTarde" style="border: 1px solid #EEEEEE;" />';
+
+// Entry
+$_['customer_code'] = 'Public Key';
+$_['customer_key'] = 'Secret Key';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Lo sentimos pero usted no tiene permiso para modificar este método de pago';
+$_['error_test_customer_code'] = 'Test Public Key';
+$_['error_test_customer_key'] = 'Test Secret Key';
+$_['error_real_customer_code'] = 'Real Public Key';
+$_['error_real_customer_key'] = 'Real Secret Key';
+
+//Tips
+$_['tip'] = 'Rembember!';
