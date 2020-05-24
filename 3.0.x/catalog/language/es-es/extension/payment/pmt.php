b8fb4ad42929e5a4fc50c3b7a9caf7a741f18d0b
pgarces <37547687+pgarcess@users.noreply.github.com>
2020-05-25 01:42:41 +0200

    Update pmt.php

--- 3.0.x/catalog/language/es-es/extension/payment/pmt.php
+++ 3.0.x/catalog/language/es-es/extension/payment/pmt.php
@@ -1,19 +1,3 @@
 <?php
 // Text
-$_['method_title'] = 'Financiación Instantánea<script type="text/javascript" src="https://cdn.pagantis.com/js/pg-v2/sdk.js"></script> 
-
-<div class="customSimulator"></div>
-<script>
-    options = {
-        //IMPORTANT: Set your public key
-        "publicKey": "%f",
-        "selector": ".customSimulator",
-        "type": pgSDK.simulator.types.PRODUCT_PAGE,
-        "skin": pgSDK.simulator.skins.BLUE,
-        "itemAmount": "%f",
-        "itemQuantity": "1",
-        "locale": "%f",
-        "country": "%f",
-    }
-    pgSDK.simulator.init(options);
-</script>';
+$_['method_title'] = 'Financiación Instantánea';
